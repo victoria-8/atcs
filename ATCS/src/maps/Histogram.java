bf733c02837721d6be5813868a6b3cd35748c600
16vl0354 <16vl0354@HSS-101-08.westwindsor.k12.nj.us>
2016-01-28 13:14:25 -0500

    histogram

---
+++ ATCS/src/maps/Histogram.java
@@ -0,0 +1,70 @@
+package maps;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class Histogram
+{
+	private Map<String,Integer> histogram = new TreeMap<String,Integer>();
+
+	public Histogram()
+	{
+	}
+
+	public Histogram(String sent)
+	{
+		String[] words = sent.split(" ");
+		for (String s : words)
+		{
+			if (!histogram.containsKey(s))
+			{
+				histogram.put(s, 1);
+			}
+			else
+			{
+				histogram.put(s,histogram.get(s)+1);
+			}
+		}
+	}
+	
+	public void setSentence(String sent)
+	{String[] words = sent.split(" ");
+	for (String s : words)
+	{
+		if (!histogram.containsKey(s))
+		{
+			histogram.put(s, 1);
+		}
+		else
+		{
+			histogram.put(s,histogram.get(s)+1);
+		}
+	}
+		
+	}
+
+	public String toString()
+	{
+		String output="";
+		
+		Iterator<Map.Entry<String, Integer>> entries = histogram.entrySet().iterator();
+		while (entries.hasNext()) {
+		    Map.Entry<String, Integer> entry = entries.next();
+		    int value = entry.getValue();
+		    System.out.print(entry.getKey() + "\t");
+
+		    for (int i = 0; i < value; i++)
+		    {
+		    	System.out.print("*");
+		    }
+		    
+		    System.out.println();
+		}
+		
+		String allStars="";
+		return "" + "\n\n";
+	}
+}
