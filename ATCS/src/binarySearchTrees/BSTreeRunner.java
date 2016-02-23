84968f24c731c1a61c54c9fe3cb52f1536601b13
16vl0354 <16vl0354@HSS-101-08.westwindsor.k12.nj.us>
2016-02-23 14:50:20 -0500

    bts

---
+++ ATCS/src/binarySearchTrees/BSTreeRunner.java
@@ -0,0 +1,30 @@
+package binarySearchTrees;
+
+import static java.lang.System.*;
+
+public class BSTreeRunner
+{
+   public static void main( String args[] )
+   {
+ 		BinarySearchTree tree = new BinarySearchTree();
+ 		tree.add(90);
+ 		tree.add(100);
+ 		tree.add(80);
+ 		tree.add(70);
+ 		tree.add(120);
+ 		tree.add(85);
+ 		tree.add(98);
+ 		tree.inOrder();
+ 		tree.preOrder();
+
+
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 		
+ 
+   }
+}
