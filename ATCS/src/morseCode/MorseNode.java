01e529266e4e2c345cfad26dca2d6d384b6f3e2e
16vl0354 <16vl0354@HSS-101-08.westwindsor.k12.nj.us>
2016-03-22 14:46:58 -0400

    morse

--- ATCS/src/morseCode/MorseNode.java
+++ ATCS/src/morseCode/MorseNode.java
@@ -6,10 +6,12 @@ public class MorseNode implements Comparable{
 	private String letter;
 	private MorseNode left;
 	private MorseNode right;
+	private int index;
 
-	public MorseNode(String s)
+	public MorseNode(String s, int index)
 	{
-		s = morse;
+		letter = s;
+		this.index = index;
 	}
 	
 	public void setLeft(MorseNode left)
@@ -29,13 +31,39 @@ public class MorseNode implements Comparable{
 	{
 		return morse;
 	}
+	
+	public Comparable getValue()
+	{
+		return letter;
+	}
+	public int getIndex()
+	{
+		return index;
+	}
+	
+	public MorseNode getLeft()
+	{
+		return left;
+	}
+	public MorseNode getRight()
+	{
+		return right;
+	}
 
 	@Override
 	public int compareTo(Object arg0) {
 		MorseNode other = (MorseNode)arg0;
-		String otherS = other.getMorse();
-		for ()
-		return 0;
+		if (index > other.getIndex())
+			return 1;
+		else if (index < other.getIndex())
+			return -1;
+		else 
+			return 0;
+	}
+	
+	public String toString()
+	{
+		return letter;
 	}
 
 }
--- ATCS/src/morseCode/MorseTree.java
+++ ATCS/src/morseCode/MorseTree.java
@@ -6,48 +6,63 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import phones.PhoneEntry;
 import binarySearchTrees.BinarySearchTree;
 import binarySearchTrees.TreeNode;
 
 public class MorseTree extends BinarySearchTree
 {
 	private MorseNode root;
+	private String[] arr;
 	public MorseTree()
 	{
 		//root = new MorseNode("start");
+		root = null;
 		String morse = "0-9O.8M Q G Z7T Y K C  N X D B6/1J W P A   R L E2- U F I3V S4H5";
+		
 		add(morse);
 	}
 	
 	public void add(String s)
 	{
-		String[] arr = new String[72];
+		String[] arr = new String[s.length()];
 		for (int i = 1; i < s.length(); i++)
 		{
 			arr[i-1] = s.substring(i-1,i);
 		}
+		root = (balance(arr));
 		
-		add(arr);
-		balance(arr);
+//		System.out.println(arr[0]);
+//		int index = 1;
+//		while (index < 72)
+//		{
+//			add(arr[index], add(arr[index-1],root,index-1), index);
+//			index++;
+//		}
+		
 	}
 	
 
-	private MorseNode add(Comparable val, MorseNode node)
+	private MorseNode add(String val, MorseNode node, int index)
 	{
-	   if(node == null)
-			node = new MorseNode(val);
+		//System.out.println(val);
+
+		if(node == null)
+		{
+			node = new MorseNode(val,index);
+		}
 	   else
 	   {
-		   Comparable treeValue = node.getValue();
-			int dirTest = val.compareTo(treeValue);
+		   MorseNode n = new MorseNode(val, index);
+			int dirTest = node.compareTo(n);
 			
 			if(dirTest <= 0)
-				node.setLeft(add(val, node.getLeft()));
+				node.setLeft(add(arr[index+1], node.getLeft(),index++));
 			else if(dirTest > 0)
-				node.setRight(add(val, node.getRight()));
+				node.setRight(add(arr[index+1], node.getRight(),index++));
 	   }
 	   
-	   return tree;
+	   return node;
 	}
 	
 	
@@ -64,14 +79,77 @@ public class MorseTree extends BinarySearchTree
 	  }
 	  
 	  int middle = start + (end-start)/2;
-      MorseNode newNode = new MorseNode(arr[middle]);
-      
-    
-      newNode.setLeft(balance(arr, start, middle-1));
-	  newNode.setRight(balance(arr, middle + 1, end));
+     MorseNode newNode = new MorseNode(arr[middle],middle);
+	  
+    	  newNode.setLeft(balance(arr, start, middle-1));
+	      newNode.setRight(balance(arr, middle + 1, end));
 	  
 	    return newNode;
     }
+	
+//	private MorseNode balance(MorseNode node, String[] arr, int start, int end)
+//	{
+//	  if (start > end)
+//	  {
+//		  return null;
+//	  }
+//	  int middle = start + (end-start)/2;
+//	  MorseNode newNode = new MorseNode(arr[middle],middle);
+//
+//	  if (root == null)
+//		  root = newNode;
+//	  else
+//	  {
+//	      //System.out.println(arr[middle]);
+//
+//	     // System.out.println(newNode);
+//	    
+//	      node.setLeft(balance(node, arr, start, middle-1));
+//		  node.setRight(balance(node, arr, middle + 1, end));
+//	  }
+//	  	System.out.println(newNode);
+//	    return newNode;
+//    }
+	public int getNumLevels()
+	{
+		return getNumLevels(root);
+	}
+
+	private int getNumLevels(MorseNode tree)
+	{
+		if(tree==null)
+			return 0;
+		else if(getNumLevels(tree.getLeft())>getNumLevels(tree.getRight()))
+			return 1+getNumLevels(tree.getLeft());
+		else
+			return 1+getNumLevels(tree.getRight());
+	}
+	public void display()
+	{
+		for (int i=0;i<getNumLevels();i++)
+		{
+			display(root,i);
+			System.out.println();
+		}
+		
+	}
+	
+	private void display(MorseNode tree, int level)
+	{
+		if (tree==null)
+			return;
+	else if(level==0)
+			System.out.print(tree + " " );
+		else
+		{
+			display(tree.getLeft(), level-1);
+			display(tree.getRight(), level-1);
+
+		}
+	}
+
+	
+
 	
 	public static void main(String[] args) throws IOException
 	{
@@ -89,7 +167,7 @@ public class MorseTree extends BinarySearchTree
 //			
 //		}
 		MorseTree tree = new MorseTree();
-		tree.levelOrderTraversal();
+		tree.display();
 	}
 
 }
