942642a82953940d0a4c6a7e9a6b0fd97e906d5b
16vl0354 <16vl0354@HSS-101-08.westwindsor.k12.nj.us>
2016-04-19 13:31:21 -0400

    graphs

--- ATCS/src/graphs/Graph.java
+++ ATCS/src/graphs/Graph.java
@@ -1,40 +1,95 @@
 package graphs;
+
+import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Graph
 {
 	private TreeMap<String, String> map;
-	private boolean yahOrNay;
+	private boolean[] checked;
 
 	public Graph(String line)
 	{
-		String[] array = line.split(" ");
-		for (String s : array)
-		{
-			if (!(map.containsKey(s.substring(0, 1))))
-			{
-				map.put(s.substring(0,1),s.substring(1,2));
+		if(map==null)
+			map=new TreeMap();
+		for(int x=0; x<line.length(); x=x+3){
+			if(!map.containsKey(line.substring(x, x+1))){
+				map.put(line.substring(x, x+1), line.substring(x+1, x+2));
 			}
-			else
-			{
-				
+			else{
+				map.replace(line.substring(x, x+1), map.get(line.substring(x, x+1))+line.substring(x+1, x+2));
+			}
+			if(!map.containsKey(line.substring(x+1, x+2))){
+				map.put(line.substring(x+1, x+2), line.substring(x, x+1));
+			}
+			else{
+				map.replace(line.substring(x+1, x+2), map.get(line.substring(x+1, x+2))+line.substring(x, x+1));
 			}
 		}
-	
 	}
 
 	public boolean contains(String letter)
 	{
-	   return true;
+	   return map.containsKey(letter);
 	}
 
-	public void check(String first, String second, String placesUsed)
-	{
+	public boolean directCheck(String first, String second){
+		if(first.equals(second))
+			return true;
+		if(!map.containsKey(first)||!map.containsKey(second)){
+			return false;
+		}
+		else{
+			CharSequence x=second;
+			return (map.get(first)).contains(x);
+		}
 	}
+	public boolean check(String first, String second, String placesUsed)
+	{
+		placesUsed+=first;
+			if(directCheck(first,second)){
+				return true;
+			}
+			else{
+				String x=map.get(first);
+				if(x==null){
+					System.out.println(first+" "+second);
+					return false;
+				}
+					
+				for(int i=0;i<x.length();i++){
+					if(!placesUsed.contains(x.substring(i,i+1))){
+						if(check(x.substring(i, i+1),second,placesUsed))
+							return true;
+					}
+							
+				}
+				return false;
+			}
+
+//		get the current list of connections for one
+//		loop through all of the connections
+//		If you have not checked the current spot
+//		add current spot to been
+//		check to see a connection exists between spot and the destination ( recursive call )
+
+		
+	}
+	
+	
 
 	public String toString()
 	{
-		return "";
+		String output="";
+		Iterator<String> a=map.keySet().iterator();
+		while(a.hasNext()){
+			String character=a.next();
+			output+=character+"\t";
+			String lim=map.get(character+"");
+			
+			output+=lim+"\n";
+		}
+		return output;
 	}
 }
--- ATCS/src/graphs/GraphRunner.java
+++ ATCS/src/graphs/GraphRunner.java
@@ -1,4 +1,5 @@
 package graphs;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -8,12 +9,19 @@ public class GraphRunner
 {
 	public static void main( String[] args ) throws IOException
 	{
-		Scanner file = new Scanner(new File("graph.dat"));
+		Scanner file = new Scanner(new File("File/graph1.dat"));
 		int howManyTimes = file.nextInt();
 		file.nextLine();
 		for(int x=0; x<howManyTimes; x++)
 		{
-			//add code here
+			Graph y=new Graph(file.nextLine());
+			String z=file.nextLine();
+			if(y.check(z.charAt(0)+"", z.charAt(1)+"", "")){
+				System.out.println();
+				System.out.println(z.charAt(0)+" connects to "+ z.charAt(1));
+			}
+			else
+				System.out.println(z.charAt(0)+" does not connect to "+ z.charAt(1));
 		}
 	}
 }
